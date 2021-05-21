3a9cb56a78acd5fe1ab44057dca05c6399427c1b
fahrenheit-fair-enough <sunkissed.032@gmail.com>
2021-05-21 19:55:07 +0900

    ActiveStorageの導入とバリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
   def index
   end
+
+
+  def new
+    @product = Producto.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
 class Product < ApplicationRecord
+  belongs_to :user
+  # has_one    :purchase_record
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_free_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  with_options presence: true do
+    validates :name
+    validates :explain
+    validates :category_id
+    validates :status_id
+    validates :shipping_free_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :price
+  end
 end
