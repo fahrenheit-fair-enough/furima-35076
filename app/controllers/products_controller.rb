133b4bc882ac4e00685a1d410f049d70a450502a
fahrenheit-fair-enough <sunkissed.032@gmail.com>
2021-05-24 18:26:46 +0900

    商品削除機能を削除ボタンで使えるようにするための実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, expect: [:index, :show]
-  before_action :select_product, only: [:show, :edit, :update]
+  before_action :select_product, only: [:show, :edit, :update, :destroy]
   before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
@@ -32,6 +32,11 @@ class ProductsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to root_path
   end
 
   private
